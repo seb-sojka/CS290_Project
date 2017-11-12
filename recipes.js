0335f0414c2d928821a3857d3e78e8ea02a45dc6
seb-sojka <sojkas@oregonstate.edu>
2017-11-11 21:00:28 -0600

    Intial Commit

---
+++ recipes.js
@@ -0,0 +1,30 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3354);
+app.use(express.static('views/images'));
+
+app.get('/',function(req,res){
+	res.render('home');
+});
+
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Website Located at http://flip2.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
